6341c94f9acba36cf823d4c203094d471ef478e9
龙天 <wyvernnot@gmail.com>
2016-06-14 11:43:15 +0800

    0.3.1

--- popup.js
+++ popup.js
@@ -1,36 +1,40 @@
-$(function () {
-  var token = localStorage["token"];
-  var width = localStorage["width"] || 600;
-  var height = localStorage["height"] || 300;
-  var invalidToken = "无效的 token";
+chrome.storage.sync.get('token', function (item) {
+  var token = item.token;
 
-  /**
-   * 验证 token 有效性
-   * @param string
-   * @returns {*|boolean}
-   */
-  function isValidToken(string) {
-    return string && /^[\w]{32}$/.test(string);
-  }
+  $(function () {
+    var width = localStorage["width"] || 600;
+    var height = localStorage["height"] || 300;
+    var invalidToken = "无效的 token";
 
-  if (isValidToken(token)) {
-    $("<iframe>").attr({
-      src: "http://cloud.oneapm.com/share/chart?token=" + token + "&spanTime=3600000&width=" + width + "&height=" + height,
-      width: width,
-      height: height,
-      frameborder: 0
-    }).appendTo("body");
-    $("body").css({
-      width: width,
-      height: height
-    });
-    chrome.runtime.sendMessage('fetch');
-  } else {
-    chrome.runtime.openOptionsPage();
-    $("body")
-      .text(invalidToken)
-      .css({
-        width: '8em'
+    /**
+     * 验证 token 有效性
+     * @param string
+     * @returns {*|boolean}
+     */
+    function isValidToken(string) {
+      return string && /^[\w]{32}$/.test(string);
+    }
+
+    if (isValidToken(token)) {
+      $("<iframe>").attr({
+        src: "http://cloud.oneapm.com/share/chart?token=" + token + "&spanTime=3600000&width=" + width + "&height=" + height,
+        width: width,
+        height: height,
+        frameborder: 0
+      }).appendTo("body");
+      $("body").css({
+        width: width,
+        height: height
       });
-  }
+      chrome.runtime.sendMessage('fetch');
+    } else {
+      chrome.runtime.openOptionsPage();
+      $("body")
+        .text(invalidToken)
+        .css({
+          width: '8em'
+        });
+    }
+  })
 })
+
